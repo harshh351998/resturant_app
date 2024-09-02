9a89353e8dd8ef51ab3685a9839db75c817925e9
harshh351998 <mendapara.harsh47@gmail.com>
2024-09-02 15:18:44 +0530

    #03:- Routing of components created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MaterialComponent } from './material/material.component';
+import { ResturantDashboardComponent } from './resturant-dashboard/resturant-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'**', redirectTo:'login', pathMatch:'full'}, 
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'dashboard',component:ResturantDashboardComponent},
+  {path:'material',component:MaterialComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
